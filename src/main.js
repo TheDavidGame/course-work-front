921810db21c7ec2a4f3bdbed9b2ccb9b869f292c
aitunin <2323353@gmail.com>
2022-03-22 00:41:35 +0300

    heroku build fix

--- src/main.js
+++ src/main.js
@@ -1,11 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import vuetify from './plugins/vuetify'
-import { loadFonts } from './plugins/webfontloader'
+import vuetify from '@/plugins/vuetify'
 import router from "@/router";
 import store from "@/store/store";
-
-loadFonts()
 
 createApp(App)
   .use(vuetify)
