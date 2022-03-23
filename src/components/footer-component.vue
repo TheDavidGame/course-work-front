c67c7c482804a5692445ca6899c66e04d0d3f9d1
aitunin <2323353@gmail.com>
2022-03-23 19:02:25 +0300

    footer icons fix

--- src/components/footer-component.vue
+++ src/components/footer-component.vue
@@ -2,14 +2,14 @@
   <v-footer class="pa-0 app-footer">
     <v-row>
       <v-spacer/>
-      <v-col sm="1">
-        <v-img max-width="70%" max-height="70%" :src="require('../assets/vk-social-logotype.png')"/>
+      <v-col sm="1" align="center">
+        <v-img class="footer-logo" :src="require('../assets/vk-social-logotype.png')"/>
       </v-col>
-      <v-col sm="1">
-        <v-img max-width="70%" max-height="70%" :src="require('../assets/instagram.png')"/>
+      <v-col sm="1" align="center">
+        <v-img class="footer-logo" :src="require('../assets/instagram.png')"/>
       </v-col>
-      <v-col sm="1">
-        <v-img max-width="70%" max-height="70%" :src="require('../assets/facebook.png')"/>
+      <v-col sm="1" align="center">
+        <v-img class="footer-logo justify-center" :src="require('../assets/facebook.png')"/>
       </v-col>
       <v-spacer/>
     </v-row>
@@ -22,10 +22,16 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
   .app-footer {
     background-color: #1D3557;
     max-height: 7%;
     min-height: 70px;
+  }
+  .footer-logo {
+    max-width: 45%;
+    min-width: 45%;
+    max-height: 45%;
+    min-height: 45%;
   }
 </style>
